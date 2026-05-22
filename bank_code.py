72e2e57244eb9d744f87a8929e37d88eabb839c5
HrickM <hrickmukherjee609@gmail.com>
2026-05-22 08:34:45 +0530

    streamlit added

--- bank_code.py
+++ bank_code.py
@@ -1,66 +1,4 @@
-# Multiple Inheritance
-
-class Bank_v1:
-    bank_name = "SBI"
-    bank_branch = "Hyderabad"
-    bank_ifsc = 1234
-
-    def __init__(self, n, a, ac, b):
-        self.name = n
-        self.age = a
-        self.account = ac
-        self.balance = b
-
-    def customer_details(self):
-        print(f"name of customer {self.name}")
-        print(f"age of customer {self.age}")
-        print(f"customer account {self.account}")
-        print(f"customer balance {self.balance}")
-
-    @classmethod
-    def bank_details(cls):
-        print(f"bank name is {cls.bank_name}")
-        print(f"bank branch is {cls.bank_branch}")
-
-
-class Bank_v2:
-    bank_manager = "Eswar"
-    bank_number = 1234567
-
-    def __init__(self, n, a, ac, b, pin):
-        super().__init__(n, a, ac, b)
-        self.pin = pin
-
-    @classmethod
-    def bank_details(cls):
-        super().bank_details()
-        print(f"bank manager is {cls.bank_manager}")
-        print(f"bank number is {cls.bank_number}")
-
-
-class Bank_v3(Bank_v2, Bank_v1):
-    bank_branch = "Vizag"
-    bank_ifsc = 12345
-
-    def __init__(self, n, a, ac, b, pin):
-        super().__init__(n, a, ac, b, pin)
-
-    @classmethod
-    def bank_details(cls):
-        super().bank_details()
-        print(f"bank ifsc is {cls.bank_ifsc}")
-
-    def customer_details(self):
-        super().customer_details()
-        print(f"customer password is {self.pin}")
-
-
-# Object Creation
-Ram = Bank_v3("Ramul", 23, 2345, 1234, 23456)
-
-# Method Calling
-Ram.customer_details()
-Ram.bank_details()
+import streamlit as st
 
 # Multiple Inheritance
 
@@ -76,15 +14,15 @@ class Bank_v1:
         self.balance = b
 
     def customer_details(self):
-        print(f"name of customer {self.name}")
-        print(f"age of customer {self.age}")
-        print(f"customer account {self.account}")
-        print(f"customer balance {self.balance}")
+        st.write(f"Name of customer: {self.name}")
+        st.write(f"Age of customer: {self.age}")
+        st.write(f"Customer account: {self.account}")
+        st.write(f"Customer balance: {self.balance}")
 
     @classmethod
     def bank_details(cls):
-        print(f"bank name is {cls.bank_name}")
-        print(f"bank branch is {cls.bank_branch}")
+        st.write(f"Bank name is: {cls.bank_name}")
+        st.write(f"Bank branch is: {cls.bank_branch}")
 
 
 class Bank_v2:
@@ -98,8 +36,8 @@ class Bank_v2:
     @classmethod
     def bank_details(cls):
         super().bank_details()
-        print(f"bank manager is {cls.bank_manager}")
-        print(f"bank number is {cls.bank_number}")
+        st.write(f"Bank manager is: {cls.bank_manager}")
+        st.write(f"Bank number is: {cls.bank_number}")
 
 
 class Bank_v3(Bank_v2, Bank_v1):
@@ -112,16 +50,27 @@ class Bank_v3(Bank_v2, Bank_v1):
     @classmethod
     def bank_details(cls):
         super().bank_details()
-        print(f"bank ifsc is {cls.bank_ifsc}")
+        st.write(f"Bank IFSC is: {cls.bank_ifsc}")
 
     def customer_details(self):
         super().customer_details()
-        print(f"customer password is {self.pin}")
+        st.write(f"Customer PIN is: {self.pin}")
 
 
-# Object Creation
-Ram = Bank_v3("Ramul", 23, 2345, 1234, 23456)
+# Streamlit UI
+st.title("Bank Management System")
 
-# Method Calling
-Ram.customer_details()
-Ram.bank_details()
+name = st.text_input("Enter Customer Name")
+age = st.number_input("Enter Age", min_value=1, step=1)
+account = st.number_input("Enter Account Number", min_value=1, step=1)
+balance = st.number_input("Enter Balance", min_value=0, step=1)
+pin = st.number_input("Enter PIN", min_value=1000, step=1)
+
+if st.button("Show Details"):
+    Ram = Bank_v3(name, age, account, balance, pin)
+
+    st.subheader("Customer Details")
+    Ram.customer_details()
+
+    st.subheader("Bank Details")
+    Ram.bank_details()
